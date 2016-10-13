58287a18cd01625ee7f04e573521ed9505ea1b24
vagrant <vagrant@localhost.localdomain>
2016-10-13 13:47:00 +0200

    primerEj.js

---
+++ primerEj.js
@@ -0,0 +1,16 @@
+function miFunConVar(){
+    var x= "Esta variable es local";
+    for(var i=0 ;i< ; ++i){
+        console.log("Valor de i: " + i);
+    }
+    console.log("valor de i: " + i);
+}
+
+function miFunConLet(){
+    let x= "Esta variable es local";
+    for(let i=0 ;i< ; ++i){
+        console.log("Valor de i: " + i);
+    }
+    //TODO: Buscar una cond para saber si existe i
+    //console.log("valor de i: " + i);
+}
